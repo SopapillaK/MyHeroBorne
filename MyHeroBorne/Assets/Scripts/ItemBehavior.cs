180e3701c63d74f3b1e82d0f367324ff5c1ce12d
SopapillaK <heyitssoph1234@gmail.com>
2022-02-12 13:45:53 -0700

    ch8 fully

--- MyHeroBorne/Assets/Scripts/ItemBehavior.cs
+++ MyHeroBorne/Assets/Scripts/ItemBehavior.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class ItemBehavior : MonoBehaviour
 {
+    public GameBehavior gameManager;
+
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameBehavior>();
+    }
+
     void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.name == "Player")
         {
             Destroy(this.transform.parent.gameObject);
             Debug.Log("Item collected!");
+
+            gameManager.Items += 1;
         }
     }
 }
